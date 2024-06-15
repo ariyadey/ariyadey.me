dd14053707218e6298542dce59c51dac46d72f28
Ariya Moheb <ariya.mms@gmail.com>
2024-06-15 17:02:53 +0330

    Fix typo for application name: ariyadey-main

--- projects/ariyadey-main/src/app/app.component.spec.ts
+++ projects/ariyadey-main/src/app/app.component.spec.ts
@@ -1,31 +1,29 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ariyaday-main' title`, () => {
+  it(`should have the 'ariyadey-main' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ariyaday-main');
+    expect(app.title).toEqual("ariyadey-main");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, ariyaday-main',
-    );
+    expect(compiled.querySelector("h1")?.textContent).toContain("Hello, ariyadey-main");
   });
 });
