28dcbc9abfb23718baa2fb3a1a9745f00a1597d0
Ariya Moheb <ariya.mms@gmail.com>
2024-06-15 14:36:53 +0330

    Add main application: ariyadey-main

---
+++ projects/ariyaday-main/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have the 'ariyaday-main' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ariyaday-main');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ariyaday-main');
+  });
+});
