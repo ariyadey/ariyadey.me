8472c90c48786e7665280cab3fe2a807ea0567ff
Ariya Moheb <ariya.mms@gmail.com>
2024-06-21 01:36:31 +0330

    Add DirectionalDirective to horizontally mirror the specified icons in RTL mode

---
+++ projects/ariyadey-main/src/app/shared/theming/directional.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DirectionalDirective } from "./directional.directive";
+
+describe("DirectionalDirective", () => {
+  it("should create an instance", () => {
+    const directive = new DirectionalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
