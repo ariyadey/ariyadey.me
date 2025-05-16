6fb5e07bcd2fc44a1e47b68a5bc1c4e70fe6e10e
Ariya Moheb <ariya.mms@gmail.com>
2025-05-16 21:37:35 +0330

    Add SkillsComponent to display categorized skills with internationalization support

---
+++ projects/ariyadey-main/src/app/components/home/skills/skills.component.ts
@@ -0,0 +1,34 @@
+import { KeyValuePipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import { MatChip, MatChipSet } from "@angular/material/chips";
+import { MatDivider } from "@angular/material/divider";
+
+import { MatListItemLine } from "@angular/material/list";
+import { I18nPipe } from "@main/shared/i18n/i18n.pipe";
+import { PersonalInfoService } from "@main/shared/personal-info/personal-info.service";
+
+@Component({
+  selector: "app-skills",
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatChipSet,
+    MatChip,
+    MatListItemLine,
+    KeyValuePipe,
+    I18nPipe,
+    MatDivider,
+  ],
+  templateUrl: "./skills.component.html",
+  styles: ``,
+})
+export class SkillsComponent {
+  readonly categorizedSkills = inject(PersonalInfoService).getSkills();
+
+  compareFn() {
+    return 0;
+  }
+}
