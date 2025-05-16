f054c8673f6058f7391e69bf33a8397010493b4d
Ariya Moheb <ariya.mms@gmail.com>
2025-05-17 02:11:39 +0330

    Add ProjectsComponent to display a list of projects with details and skills

---
+++ projects/ariyadey-main/src/app/components/home/projects/projects.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from "@angular/core";
+import { MatIconAnchor } from "@angular/material/button";
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
+import { MatChip, MatChipSet } from "@angular/material/chips";
+import { MatDivider } from "@angular/material/divider";
+import { MatIcon } from "@angular/material/icon";
+import { DateRangePipe } from "@main/shared/i18n/date-range.pipe";
+import { I18nPipe } from "@main/shared/i18n/i18n.pipe";
+import { PersonalInfoService } from "@main/shared/personal-info/personal-info.service";
+
+@Component({
+  selector: "app-projects",
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatChipSet,
+    MatChip,
+    MatIcon,
+    MatIconAnchor,
+    I18nPipe,
+    MatDivider,
+    DateRangePipe,
+  ],
+  templateUrl: "./projects.component.html",
+  styles: ``,
+})
+export class ProjectsComponent {
+  readonly projects = inject(PersonalInfoService).getProjects();
+}
