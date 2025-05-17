c8fb9cc6667cf0083921ff18cc1e22db18735d8d
Ariya Moheb <ariya.mms@gmail.com>
2025-05-17 04:37:44 +0330

    Remove unused Transloco locale provider from app configuration

--- projects/ariyadey-main/src/app/app.config.ts
+++ projects/ariyadey-main/src/app/app.config.ts
@@ -10,7 +10,6 @@ import { MAT_CARD_CONFIG, MatCardConfig } from "@angular/material/card";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { provideRouter, withInMemoryScrolling, withRouterConfig } from "@angular/router";
 import { provideTransloco, TranslocoService } from "@jsverse/transloco";
-import { provideTranslocoLocale } from "@jsverse/transloco-locale";
 import { TranslocoHttpLoader } from "@main/shared/i18n/transloco-http-loader";
 import { forkJoin, tap } from "rxjs";
 import { routes } from "./app.routes";
@@ -45,17 +44,8 @@ export const appConfig: ApplicationConfig = {
         availableLangs: ["en", "fa"],
         defaultLang: "en",
         prodMode: !isDevMode(),
-        flatten: {
-          aot: !isDevMode(),
-        },
       },
       loader: TranslocoHttpLoader,
-    }),
-    provideTranslocoLocale({
-      langToLocaleMapping: {
-        en: "en-us",
-        fa: "fa-ir",
-      },
     }),
     { provide: Window, useValue: window },
     { provide: MAT_CARD_CONFIG, useValue: { appearance: "raised" } as MatCardConfig },
