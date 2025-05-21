8c720ad8269492c3e64661524926e41f00ebfeb9
Ariya Moheb <ariya.mms@gmail.com>
2025-05-21 12:57:46 +0330

    Fix TruncatableDirective import

--- projects/ariyadey-main/src/app/components/home/summary/summary.component.ts
+++ projects/ariyadey-main/src/app/components/home/summary/summary.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from "@angular/core";
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
 import { I18nPipe } from "@main/shared/i18n/i18n.pipe";
+import { TruncatableDirective } from "@main/shared/layout/truncatable.directive";
 import { PersonalInfoService } from "@main/shared/personal-info/personal-info.service";
-import { TruncatableDirective } from "@main/shared/truncatable.directive";
 
 @Component({
   selector: "app-summary",
