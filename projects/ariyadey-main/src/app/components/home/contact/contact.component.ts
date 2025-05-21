ebccde335b3b36e4127bde09600761bd9ade08e5
Ariya Moheb <ariya.mms@gmail.com>
2025-05-21 12:57:02 +0330

    Refactor ContactComponent to use EmailComponent and CommunicationChannelsComponent for contact display

--- projects/ariyadey-main/src/app/components/home/contact/contact.component.ts
+++ projects/ariyadey-main/src/app/components/home/contact/contact.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject } from "@angular/core";
-import { MatAnchor, MatIconAnchor } from "@angular/material/button";
+import { Component } from "@angular/core";
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from "@angular/material/card";
-
-import { MatIcon } from "@angular/material/icon";
+import { CommunicationChannelsComponent } from "@main/shared/components/contact/communication-channels/communication-channels.component";
+import { EmailComponent } from "@main/shared/components/contact/email/email.component";
 import { I18nPipe } from "@main/shared/i18n/i18n.pipe";
-import { PersonalInfoService } from "@main/shared/personal-info/personal-info.service";
 
 @Component({
   selector: "app-contact",
@@ -13,14 +11,11 @@ import { PersonalInfoService } from "@main/shared/personal-info/personal-info.se
     MatCardHeader,
     MatCardContent,
     MatCardTitle,
-    MatIcon,
-    MatAnchor,
-    MatIconAnchor,
     I18nPipe,
+    CommunicationChannelsComponent,
+    EmailComponent,
   ],
   templateUrl: "./contact.component.html",
   styles: ``,
 })
-export class ContactComponent {
-  readonly contacts = inject(PersonalInfoService).getContacts();
-}
+export class ContactComponent {}
