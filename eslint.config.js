33bab6568fa99ad6f11b6526bc4e209c22f2a7c7
Ariya Moheb <ariya.mms@gmail.com>
2025-06-03 09:28:38 +0330

    Enhance ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,31 @@
-// @ts-check
+// TODO:
+//  - Set up ESLint for JSON, CSS and Markdown:
+//  https://github.com/eslint
+//  - Integrate ESLint with Browserslist:
+//  https://web.dev/articles/use-baseline-with-browserslist
+//  - Integrate ESLint with Baseline:
+//  https://web.dev/blog/eslint-baseline-integration
 const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
+const tslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 const eslintConfigPrettier = require("eslint-config-prettier");
 
-module.exports = tseslint.config(
+module.exports = tslint.config(
   {
     files: ["**/*.ts"],
     extends: [
       eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
+      ...tslint.configs.recommended,
+      ...tslint.configs.stylistic,
       ...angular.configs.tsRecommended,
       eslintConfigPrettier,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@typescript-eslint/array-type": ["warn", { default: "generic", readonly: "generic" }],
-      "@typescript-eslint/no-unused-expressions": ["warn", { allowTernary: true }],
-      "@typescript-eslint/no-unused-vars": ["off"],
-      "@typescript-eslint/consistent-type-definitions": ["off"],
+      "@typescript-eslint/array-type": ["error", { default: "generic", readonly: "generic" }],
+      "@typescript-eslint/no-unused-expressions": ["error", { allowTernary: true }],
+      "@typescript-eslint/no-unused-vars": ["off"], // Delegated to TS compiler
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
       "@angular-eslint/component-selector": [
         "error",
         {
@@ -35,12 +41,13 @@ module.exports = tseslint.config(
           style: "camelCase",
         },
       ],
-      "@angular-eslint/template/eqeqeq": ["off", { allowNullOrUndefined: true }],
     },
   },
   {
     files: ["**/*.html"],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/eqeqeq": ["error", { allowNullOrUndefined: true }],
+    },
   },
 );
