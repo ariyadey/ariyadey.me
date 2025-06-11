aa657b52a732dc663bb8ecbfc9fcaf38ee020c6b
Ariya Moheb <ariya.mms@gmail.com>
2025-06-11 07:55:43 +0330

    Remove trailing slashes from sitemap URLs

--- generate-sitemp.js
+++ generate-sitemp.js
@@ -4,8 +4,8 @@ const { SitemapStream } = require("sitemap");
 const BASE_URL = "https://ariyadey.me";
 const OUTPUT_DIR = "dist/ariyadey-main/browser/sitemap.xml";
 const LINKS = [
-  { url: "/en/", changefreq: "weekly", priority: 1.0 },
-  { url: "/fa/", changefreq: "weekly", priority: 1.0 },
+  { url: "/en", changefreq: "weekly", priority: 1.0 },
+  { url: "/fa", changefreq: "weekly", priority: 1.0 },
 ];
 
 const sitemap = new SitemapStream({ hostname: BASE_URL });
